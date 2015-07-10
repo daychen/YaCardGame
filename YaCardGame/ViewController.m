a22218d31f5e68a1fcf0e58224bcf2948e04ecde
Chen <Day@chn-dachen-mac.local>
2015-07-10 03:14:37 -0700

    add element in storyboard

--- YaCardGame/ViewController.m
+++ YaCardGame/ViewController.m
@@ -9,10 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property ( nonatomic) int flipcount;
+
 
 @end
 
 @implementation ViewController
+
+-(void )setFlipcount:(int)flipcount{
+    _flipcount=flipcount;
+    self.flipsLabel.text=[NSString stringWithFormat:@"flips:%d",self.flipcount];
+}
+
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+
+    if ([sender.currentTitle length]) {
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        [sender setBackgroundImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+    
+    }else{
+        [sender setTitle:@"A" forState:UIControlStateNormal];
+        
+        [sender setBackgroundImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+    }
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
