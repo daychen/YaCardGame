46915daa0c7af4cf8aa31c4ec81db5debc3b3eee
Chen <Day@chn-dachen-mac.local>
2015-07-10 02:11:51 -0700

    Initial Commit

---
+++ YaCardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YaCardGame
+//
+//  Created by Chen, Ya on 7/10/15.
+//  Copyright (c) 2015 Chen, Ya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YaCardGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YaCardGame
+//
+//  Created by Chen, Ya on 7/10/15.
+//  Copyright (c) 2015 Chen, Ya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
